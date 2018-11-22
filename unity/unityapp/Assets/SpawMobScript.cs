95bd59b7782034421e1893e8219a34e9d1dd2323
Robin Chaperon <robinchap120@gmail.com>
2018-11-22 12:32:57 +0100

    tu me casse les burnes

---
+++ unity/unityapp/Assets/SpawMobScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawMobScript : MonoBehaviour {
+    float timer = 0;
+    List<GameObject> ListMob = new List<GameObject>();
+    public float frequence;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer >= frequence)
+        {
+            timer = 0;
+            frequence = Random.Range(10, 30);
+            ListMob[Random.Range(0, ListMob.Count)].SetActive(true);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+}
