67495b3ab8796cdf60a689c6875dd92a804f83d1
Robin Chaperon <robinchap120@gmail.com>
2018-11-22 11:25:49 +0100

    tu me casse les couilles

---
+++ unity/unityapp/Assets/bulletSprite.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletSprite : MonoBehaviour {
+
+    float timer = 0;
+    public float thrust = 1;
+    Rigidbody RB;
+    Transform bT;
+    Vector3 forward;
+
+	// Use this for initialization
+	void Start () {
+        RB = this.GetComponent<Rigidbody>();
+        bT = this.GetComponent<Transform>();
+
+        forward = bT.forward;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer >= 10)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
+        RB.AddForce(forward * thrust);
+
+        bT.localEulerAngles += new Vector3(0, 0, 1);
+
+	}
+}
